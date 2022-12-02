f67b96a6abf4fa78d958ceb4a5d5ebd1a2fc907f
Alyx Ferrari <alyx.ferrari@gmail.com>
2022-12-02 14:37:29 -0500

    Initial Commit

---
+++ REDSwift/REDSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  REDSwiftApp.swift
+//  REDSwift
+//
+//  Created by Tarball on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct REDSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
