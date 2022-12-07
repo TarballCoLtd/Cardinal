240b44f0bd3b1ccfaa8ad84b1968a2742877a6d3
Alyx Ferrari <alyx.ferrari@gmail.com>
2022-12-07 11:38:47 -0500

    more organization

---
+++ REDSwift/Tabs/Search/Requests/RequestResultsView.swift
@@ -0,0 +1,14 @@
+//
+//  RequestResultsView.swift
+//  REDSwift
+//
+//  Created by Tarball on 12/7/22.
+//
+
+import SwiftUI
+
+struct RequestResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
